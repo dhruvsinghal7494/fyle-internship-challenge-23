67f8a62d8fc070d7bd35c00d5f87fabdc8f67413
Dhruv <dhruvsinghal1973@email.com>
2024-04-15 20:27:23 +0530

    api and env

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,13 @@ import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
- accessToken = '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'; 
+ accessToken = environment.githubAccessToken; 
   private readonly baseUrl = 'https://api.github.com';
 
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+  githubAccessToken: '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'
+};
