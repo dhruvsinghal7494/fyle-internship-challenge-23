7ca63d0990551363b0c80b6cc747e617b4bff362
Dhruv <dhruvsinghal1973@email.com>
2024-04-15 22:07:35 +0530

    vercel

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-  githubAccessToken: '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'
+  githubAccessToken: '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'
 };
